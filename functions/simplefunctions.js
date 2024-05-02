55f36f0ea999d866d2ac9baf5291266be1409431
Rene Banuelos <rene.b.arellano7@gmail.com>
2024-05-02 15:32:06 -0700

    Create simple functions
    Create simple functions

---
+++ functions/simplefunctions.js
@@ -0,0 +1,18 @@
+"use strict"
+
+function favoriteThings(){
+    console.log("my name is Rene Banuelos. ")
+    console.log("My favorite movie is creed.")
+    console.log("And i dont really have a favorite musician.")
+}
+favoriteThings();
+
+function whyImHere() {
+    console.log("i joined the learn to code academy for various reason, one being I'm hoping to become a well seasoned developer")
+}
+
+whyImHere();
+function favoritePlace(){
+    console.log("One of my favorite places to visit is Sedona, I find peace being in nature")
+}
+favoritePlace();
